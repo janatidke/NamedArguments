189991c539de1e9226c286d7bcd438bbf9f5acf9
Janabai Tukaram Tidke <janatidke2407@gmail.com>
2021-08-31 22:00:16 +0530

    Add project files.

---
+++ NamedArguments/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NamedArguments
+{
+    class Program
+    {
+       public class Product
+        {
+            int ordernum;
+            string productname;
+            string sellername;
+            public void Printorderdetails(int ordernum,string productname,string sellername)
+            {
+                Console.WriteLine("y {0},{1},{2}",ordernum,productname,sellername);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Product obj = new Product();
+            obj.Printorderdetails(ordernum:8,productname:"Laptop",sellername:"Abc");
+        }
+    }
+}
